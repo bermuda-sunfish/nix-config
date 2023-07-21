0df12e16943cd7fa31c3f0a5052ad319e3dbbde3
Bermuda Sunfish <bermuda.sunfish@optimanalytics.com>
2023-07-21 16:47:26 +0100

    fix home-manager version

--- home-manager/rfilipenko/766f703131.nix
+++ home-manager/rfilipenko/766f703131.nix
@@ -6,4 +6,7 @@
 
   colorscheme = inputs.nix-colors.colorschemes.atelier-sulphurpool;
   #wallpaper = outputs.wallpapers.aenami-northwind;
+
+  # https://nixos.wiki/wiki/FAQ/When_do_I_update_stateVersion
+  home.stateVersion = "23.05";
 }
