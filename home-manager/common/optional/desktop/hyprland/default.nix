bd96e91b5bb62bb7ea8aa393fdfbd8f23e1654f9
Bermuda Sunfish <bermuda.sunfish@optimanalytics.com>
2023-07-21 16:35:49 +0100

    Disabled hyprland

--- home-manager/common/optional/desktop/hyprland/default.nix
+++ home-manager/common/optional/desktop/hyprland/default.nix
@@ -34,7 +34,7 @@
   });
 
   wayland.windowManager.hyprland = {
-    #enable = true;
+    enable = true;
     package = inputs.hyprland.packages.${pkgs.system}.default;
     extraConfig =
       (import ./monitors.nix {
